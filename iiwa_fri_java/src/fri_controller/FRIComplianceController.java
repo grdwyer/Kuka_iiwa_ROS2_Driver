49a0ed85f51ad7d09def82a2daec940f4ac9bca7
George Dwyer <george.dwyer.14@ucl.ac.uk>
2018-03-21 14:25:07 +0000

    Added readable status updates to the kuka on the smartpad, and added a null space stiffness parameter for the compliance control mode

--- iiwa_fri_java/src/fri_controller/FRIComplianceController.java
+++ iiwa_fri_java/src/fri_controller/FRIComplianceController.java
@@ -4,6 +4,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
 import com.kuka.common.ThreadUtil;
+import com.kuka.connectivity.fastRobotInterface.FRIChannelInformation;
 import com.kuka.connectivity.fastRobotInterface.FRIConfiguration;
 import com.kuka.connectivity.fastRobotInterface.FRIJointOverlay;
 import com.kuka.connectivity.fastRobotInterface.FRISession;
@@ -22,6 +23,7 @@ import com.kuka.roboticsAPI.uiModel.userKeys.UserKeyAlignment;
 import com.kuka.roboticsAPI.uiModel.userKeys.UserKeyEvent;
 import com.kuka.roboticsAPI.uiModel.userKeys.UserKeyLED;
 import com.kuka.roboticsAPI.uiModel.userKeys.UserKeyLEDSize;
+import com.kuka.roboticsAPI.executionModel.CommandInvalidException;
 
 /**
  * Creates a FRI Session.
@@ -34,7 +36,7 @@ public class FRIComplianceController extends RoboticsAPIApplication
     private int send_period_, port_;
     private boolean motion_on_ = true;
     private Tool tool_;
-    private double transl_stiff_, rot_stiff_, transl_damp_, rot_damp_;
+    private double transl_stiff_, rot_stiff_, transl_damp_, rot_damp_, null_stiff_, null_damp_;
     
     @Override
     public void initialize()
@@ -52,8 +54,10 @@ public class FRIComplianceController extends RoboticsAPIApplication
         port_ = getApplicationData().getProcessData("FRI_Port").getValue();
         
         transl_stiff_ = getApplicationData().getProcessData("transl_stiffness").getValue();
+        null_stiff_ = getApplicationData().getProcessData("null_stiffness").getValue();
         rot_stiff_ = getApplicationData().getProcessData("rot_stiffness").getValue();
         transl_damp_ = getApplicationData().getProcessData("transl_damping").getValue();
+        null_damp_ = getApplicationData().getProcessData("null_damping").getValue();
         rot_damp_ = getApplicationData().getProcessData("rot_damping").getValue();
     }
     
@@ -89,6 +93,7 @@ public class FRIComplianceController extends RoboticsAPIApplication
         FRISession friSession = new FRISession(friConfiguration);
         FRIJointOverlay jointOverlay = new FRIJointOverlay(friSession);
 
+        PositionHold pos_hold;
         // wait until FRI session is ready to switch to command mode
         try
         {
@@ -100,33 +105,44 @@ public class FRIComplianceController extends RoboticsAPIApplication
 	        // set to null motion
 	        //PositionControlMode ctr_mode = new PositionControlMode();
 	        CartesianImpedanceControlMode ctr_mode = new CartesianImpedanceControlMode();
-	        
 	        ctr_mode.parametrize(CartDOF.TRANSL).setStiffness(transl_stiff_);
 	        ctr_mode.parametrize(CartDOF.TRANSL).setDamping(transl_damp_);
 	        
 	        ctr_mode.parametrize(CartDOF.ROT).setStiffness(rot_stiff_);
 	        ctr_mode.parametrize(CartDOF.ROT).setDamping(rot_damp_);
 	        
-	        PositionHold pos_hold = new PositionHold(ctr_mode, -1, null);
+	        ctr_mode.setNullSpaceStiffness(null_stiff_);
+	        ctr_mode.setNullSpaceDamping(null_damp_);
+	        
+	        pos_hold = new PositionHold(ctr_mode, -1, null);
 	        
 	        IMotionContainer positionHoldContainer = tool_.moveAsync(pos_hold.addMotionOverlay(jointOverlay));
+	        
 	        int i = 0;
 	        while(motion_on_ == true){
-	        	if(i % 30 == 0){
-	        		getLogger().info(friSession.getFRIChannelInformation().toString());
+	        	if(i % 60 == 0){
+	        		FRIChannelInformation info = friSession.getFRIChannelInformation();
+	        		getLogger().info(String.format("Current Session: %s\nCurrent Jitter: %f\nCurrent Latency: %f\nCurrent Quality: %s", info.getFRISessionState().toString(), info.getJitter(), info.getLatency(), info.getQuality().toString()));
 	        	}
 	        	ThreadUtil.milliSleep(100);
 	        	i++;
 	        }
 	        
+	        friSession.close();
 	        positionHoldContainer.cancel();
         }
         catch (final TimeoutException e)
         {
             getLogger().error(e.getLocalizedMessage());
             friSession.close();
-            return;
+            
         }
+        catch (CommandInvalidException e){
+        	getLogger().error(e.getLocalizedMessage());
+            friSession.close();
+            
+        }
+        
         finally{
         // done
         	friSession.close();
